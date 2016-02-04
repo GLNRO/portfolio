2efa8413d492e8d379caad72b6b02ec7f3fe274a
GLNRO <lg@glnro.com>
2016-02-04 00:24:17 -0500

    Threejs

--- pages/scripts/main.js
+++ pages/scripts/main.js
@@ -1,50 +1,52 @@
-var scene, camera, renderer;
+$(function (){
 
-init();
-animate();
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-function init() {
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setClearColorHex(0xEEEEEE);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-  scene = new THREE.Scene();
-  var WIDTH = window.innerWidth()
-  var HEIGHT = window.innerHeight();
+  var axes = new THREE.AxisHelper(20);
+  scene.add(axes);
 
-  renderer = new THREE.WebGLRenderer({antialias:true})
-  renderer.setSize(WIDTH,HEIGHT);
-  document.body.appendChild(renderer.domElement);
+  var planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
+  var planeMaterial = new THREE.MeshBasicMaterial({color:0xcccccc});
+  var plane = new THREE.Mesh(planeGeometry,planeMaterial);
 
-  camera = new THREE.PerspectiveCamera(45, WIDTH/HEIGHT, 0.1, 20000);
-  camera.position.set(0,6,0);
-  scene.add(camera)
+  plane.rotation.x = -0.5*Math.PI;
+  plane.postion.x = 15;
+  plane.position.y = 0;
+  plane.position.z = 0;
 
-  window.addEventListener('resize', function(){
-    var  WIDTH = window.innerWidth;
-    var  HEIGHT = window.innerHeight;
-    renderer.setSize(WIDTH, HEIGHT);
-    camera.aspect = WIDTH/HEIGHT;
-    camera.updateProjectionMatrix();
-  });
+  scene.add(plane);
 
-  renderer.setClearColorHex(0x333F47, 1);
+  var cubeGeometry = new THREE.CubeGeometry(4,4,4);
+  var cubeMAterial = new THREE.MeshBasicMaterial({color:0xff0000, wireframe: true});
+  var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-  var light = new THREE.PointLight(0xffffff);
-  light.position.set(-100,200,100);
-  scene.add(light);
+  cube.position.x=-4;
+  cube.position.y = 3;
+  cube.position.z = 0;
 
-  var loader = new THREE.JSONLoader();
-  loader.load("treehouse_logo.js",function(geometry){
-    var material = new THREE.MeshLambertMaterial({color:0x55B663});
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-  });
+  scene.add(cube);
 
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  var sphereGeometry = new THREE.SphereGeometry(4,20,20);
+  var sphereMaterial = new THREE.MeshBasicMaterial({color:0x7777ff, wirefram: true});
+  var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-}
+  sphere.position.x = 20;
+  sphere.position.y = 4;
+  sphere.position.z = 2;
 
-function animate(){
-  requestAnimationFrame(animate);
+  scene.add(sphere);
 
+  camera.position.x = -30;
+  camera.position.y = 40;
+  camera.position.z = 30;
+  camera.lookAt(scene.position);
+
+  $("#WebGL-output").append(renderer.domElement);
   renderer.render(scene,camera);
-  controls.update();
-}
+
+})
