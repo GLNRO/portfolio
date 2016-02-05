f9969be763b42a66c5412c8ba4740c4e89cc74bd
GLNRO <lg@glnro.com>
2016-02-05 00:46:49 -0500

    Create file structure for multiple scenes

--- pages/scripts/page2.js
+++ pages/scripts/page2.js
@@ -1,4 +1,5 @@
 $(function(){
+   if (document.getElementById('WebGL-output-2')) {
 
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -8,11 +9,11 @@ $(function(){
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMapEnabled = true;
 
-    var axes = new THREE.AxisHelper(20);
+     var axes = new THREE.AxisHelper(20);
     scene.add(axes);
 
     var planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
-    var planeMaterial = new THREE.MeshLambertMaterial({color:0xffffff});
+    var planeMaterial = new THREE.MeshLambertMaterial({color:0x7777ff});
     var plane = new THREE.Mesh(planeGeometry,planeMaterial);
 
     plane.rotation.x = -0.5*Math.PI;
@@ -24,29 +25,23 @@ $(function(){
 
     scene.add(plane);
 
+
     var cubeGeometry = new THREE.CubeGeometry(4,4,4);
     var cubeMaterial = new THREE.MeshLambertMaterial({color:0xff0000});
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-    cube.position.x = -4;
-    cube.position.y = 3;
+    cube.position.x = 0;
+    cube.position.y = 2;
     cube.position.z = 0;
 
     cube.castShadow = true;
+    scene.add(cube)
 
-    scene.add(cube);
 
-    var sphereGeometry = new THREE.SphereGeometry(4,20,20);
-    var sphereMaterial = new THREE.MeshLambertMaterial({color:0x7777ff, wireframe: true});
-    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-    sphere.position.x = 20;
-    sphere.position.y = 4;
-    sphere.position.z = 2;
+    var ambientLight = new THREE.AmbientLight(0x0c0c0c);
+    scene.add(ambientLight);
 
-    sphere.castShadow = true;
-
-    scene.add(sphere);
 
     var spotLight = new THREE.SpotLight( 0xffffff );
     spotLight.position.set( -40, 60, -10);
@@ -58,30 +53,31 @@ $(function(){
     camera.position.z = 30;
     camera.lookAt(scene.position);
 
-    var controls = new function(){
-      this.rotationSpeed = 0.02;
-      this.bouncingSpeed =0.03;
+
+    function addCube(){
+
+      var cubeSize = Math.ceil((Math.random() * 3));
+      var cubeGeometry = new THREE.CubeGeometry(cubeSize, cubeSize, cubeSize);
+      var cubeMaterial = new THREE.MeshLambertMaterial({color: Math.random() * 0xffffff});
+      cube.castShadow=true;
+      cube.name = "cube-" + scene.children.length;
+      cube.position.x = -30 + Math.round(Math.random()*planeGeometry.width));
+      cube.position.y = Math.round(Math.random()*5));
+      cube.position.z = -20 + Math.round(Math.random()*planeGeometry.height));
+
+      scene.add(cube);
+      this.numberOfObjects = scene.children.length;
     }
 
-    var gui = new dat.GUI();
-    gui.add(controls, 'rotationSpeed', 0, 0.5);
-    gui.add(controls, 'bouncingSpeed', 0, 0.5);
-
-
-    var step = 0;
 
     function render(){
-      step += controls.bouncingSpeed;
-      sphere.position.x = 20+(10*(Math.cos(step)));
-      sphere.position.y = 2 +(10*Math.abs(Math.sin(step)));
-      cube.rotation.x += controls.rotationSpeed;
-      cube.rotation.y += controls.rotationSpeed;
-      cube.rotation.z += controls.rotationSpeed;
       requestAnimationFrame(render);
       renderer.render(scene,camera);
     }
 
 
-    $('#WebGL-output').append(renderer.domElement);
+    $('#WebGL-output-2').append(renderer.domElement);
     render();
+
+   }
 });
